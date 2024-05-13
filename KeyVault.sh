0e50f6c52aefc7a0ab6bb16e547b310ff05b5146
Naoufal Guendouz <108728074+Nohrer@users.noreply.github.com>
2024-05-13 22:28:51 +0100

    Update KeyVault.sh

--- KeyVault.sh
+++ KeyVault.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set default values
+vaultName="KeyVault"
+include_special_characters=false
+
 # Function to generate a strong password with special characters
 generate_password_special_characters() {
     cat /dev/urandom | tr -dc '[:graph:]' | fold -w 15 | head -n 1
@@ -23,45 +27,74 @@ show_password() {
 
 # Function to check if a file already exists
 check_existence() {
-    local fileSearch=$(sudo find "$vaultName" -type f -name "$websiteName*")
+    local fileSearch=$(sudo find "$vaultName" -type f -name "$websiteName.txt")
     if [ ! -z "$fileSearch" ]; then
         echo "A file with name '$websiteName' already exists."
         exit 1
     fi
 }
 
+delete_password(){
+    local fileSearch=$(sudo find "$vaultName" -type f -name "$websiteName.txt")
+    if [ ! -z "$fileSearch" ]; then
+        echo "Are you sure you want to delete this file [$fileSearch]"
+        read -p "Y/n: " response
+        case "$response" in
+            [yY])
+                sudo rm "$fileSearch"
+                echo "Deleting file..."
+                exit 0
+                ;;
+            [nN])
+                echo "Canceled."
+                ;;
+            *)
+                echo "Invalid input."
+                ;;
+        esac
+        exit 1
+    fi
+}
+
+
 # Help menu
 print_help() {
-    echo "Usage: $0 <website_name> [options]"
+    echo "Usage: $0 [options] <website_name> "
     echo "Options:"
     echo "  -s         Show password for the given website name"
     echo "  -c         Include special characters in the generated password"
+    echo "  -d         Delete password file from the vault"
     echo "  -h         Display this help menu"
 }
 
 # Check if a website name is provided
-if [ -z "$1" ]; then
-    print_help
-    exit 1
+
+if [ ! -d "$vaultName" ]; then
+    mkdir -p "$vaultName"
 fi
-
-# Set default values
-vaultName=".KeyVault"
-websiteName="$1"
-include_special_characters=false
-
+    
 # Parse command-line options
-while getopts "sch" opt; do
+while getopts "schd" opt; do
     case $opt in
         s)
+            shift
+            websiteName="$1"
             show_password
             exit 0
             ;;
         c)
             include_special_characters=true
+            shift
             ;;
         h)
             print_help
+            shift
+            exit 0
+            ;; 
+        d)
+            shift
+            websiteName="$1"
+            delete_password
             exit 0
             ;;
         \?)
@@ -72,9 +105,13 @@ while getopts "sch" opt; do
     esac
 done
 
-# Check if file already exists
-check_existence
+if [ -z "$1" ]; then
+    print_help
+    exit 1
+fi
 
+
+websiteName="$1"
 # Generate password
 if [ "$include_special_characters" = true ]; then
     password="$(generate_password_special_characters)"
@@ -84,6 +121,10 @@ fi
 
 # Store password in a file
 file="$vaultName/$websiteName.txt"
+
+check_existence
+
+
 touch "$file"
 echo "$password" >> "$file"
 chmod 600 "$file"
